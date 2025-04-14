030d40c554c6e0026fb4412627d4e1b1c624fce9
Martin Nowak <code@dawg.eu>
2025-04-14 16:26:33 +0200

    kernel trap handler

--- kernel.c
+++ kernel.c
@@ -32,12 +32,9 @@ void putchar(char ch) {
 
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+    PANIC("unreachable here!");
 }
 
 __attribute__((section(".text.boot")))
